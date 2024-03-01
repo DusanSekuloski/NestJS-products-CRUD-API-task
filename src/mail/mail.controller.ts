2264f302b7489ea21f0b5f869bcb43ad78d456b9
Dule2001 <dusansekuloski01@gmail.com>
2024-03-01 15:49:02 +0100

    feat/NPT-3: created dynamic mail template

--- src/mail/mail.controller.ts
+++ src/mail/mail.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { MailService } from './mail.service';
+import SMTPTransport from 'nodemailer/lib/smtp-transport';
+import { SendEmailDto } from './dto/sendEmailDto';
 
 @Controller('mail')
 export class MailController {
   constructor(private readonly mailService: MailService) {}
 
-  @Get('send')
-  sendMail(): void {
-    return this.mailService.sendMail();
+  @Post('send')
+  sendMail(@Body() dto: SendEmailDto): Promise<SMTPTransport.SentMessageInfo> {
+    return this.mailService.sendMail(dto);
   }
 }
--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -2,15 +2,27 @@ import { Module } from '@nestjs/common';
 import { MailController } from './mail.controller';
 import { MailService } from './mail.service';
 import { MailerModule } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { join } from 'path';
 
 @Module({
   imports: [
     MailerModule.forRoot({
       transport: {
-        host: 'smtp.gmail.com',
+        host: process.env.MAIL_HOST,
+        port: Number(process.env.MAIL_PORT),
+        secure: true,
         auth: {
-          user: 'ledufdu01@gmail.com',
-          pass: 'prjw ulrl eisc drxc',
+          user: process.env.MAIL_USERNAME,
+          pass: process.env.MAIL_PASSWORD,
+        },
+      },
+      defaults: { from: process.env.DEFAULT_NAME_FROM },
+      template: {
+        dir: join(__dirname, '../mail/templates'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
         },
       },
     }),
