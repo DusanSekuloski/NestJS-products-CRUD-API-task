87c271f969bd2fa0a420ded27447952c940f0600
Dule2001 <dusansekuloski01@gmail.com>
2024-03-04 17:47:24 +0100

    feat/NPT-5: WIP, implemented magic login link

---
+++ src/auth/strategies/magic-login-strategy.ts
@@ -0,0 +1,14 @@
+import { PassportStrategy } from '@nestjs/passport';
+import Strategy from 'passport-magic-login';
+
+export class MagicLoginStrategy extends PassportStrategy(
+  Strategy,
+  'magic-login',
+) {
+  constructor() {
+    super({
+      secret: `${process.env.ACCESS_TOKEN_SECRET}`,
+      jwtOptions: { expiresIn: '5min' },
+    });
+  }
+}
